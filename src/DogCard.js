b0d0b57031f9282c712044545c19e5fd6837966a
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-06-07 13:24:13 -0600

    initial commit

---
+++ src/DogCard.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function DogCard(props){
+   return( 
+    <div>
+        {props.denver.map(dogUrl => {
+            {console.log("++++++++++") }
+            return <img src={dogUrl} className="dogpic" />
+        })}
+    </div>
+   )
+};
+
+export default DogCard;
